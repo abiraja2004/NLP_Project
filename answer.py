70ef0c3d6009d77b83b1fd834f64ad4366662489
Yue <yuey2@andrew.cmu.edu>
2017-11-29 18:51:08 -0500

    changed answer to "how many" questions.

--- answer.py
+++ answer.py
@@ -1,18 +1,19 @@
-import nltk,math,re
+import nltk,math,string
 from bm25 import *
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
 from nltk.tag import StanfordNERTagger
+from nltk.parse.stanford import StanfordDependencyParser
 from nltk.stem.porter import *
+from nltk import pos_tag
 from itertools import groupby
 
 """
 TODO: 1. Invent something for when, who, where to correctly extract the NE
             maybe dependency parsing
-      2. Make NE a parameter (maybe just with a higher lambda)
 
       3. Which
-      4. How many 
+      4. add preposition into where
 
       5. Reference
       6. Metadata
@@ -23,6 +24,10 @@ stemmer = PorterStemmer()
 
 NERTagger = StanfordNERTagger(model_filename = '/Users/jhl/Desktop/stanford-ner-2017-06-09/classifiers/english.muc.7class.distsim.crf.ser.gz',\
                                path_to_jar = '/Users/jhl/Desktop/stanford-ner-2017-06-09/stanford-ner.jar')
+
+depParse =StanfordDependencyParser("/Users/jhl/Desktop/stanford-parser-full-2017-06-09/stanford-parser.jar",
+            '/Users/jhl/Desktop/stanford-parser-full-2017-06-09/stanford-parser-3.8.0-models.jar')
+
 
 YESNOSTART = set(["is",'are','am','was','were',
                 'do','does','did','has','have','had','can',
@@ -45,7 +50,7 @@ ARTICLETYPES = {
 }
 
 PRONOUNS = {'He','he','She','she','it','It','They','they','You','you'}
-
+PEOPLEPRO =  {'He','he','She','she','They','they','You','you'}
 def similarity(s1,s2,idfDict):
     # helper function for tf-idf to calculate the cos similiarity
     a = set(s1)
@@ -121,21 +126,26 @@ def best5(file,question):
     docLength = 0
     idfDict = getIdfs(file)
     sims = []
-    question = [stemmer.stem(w) for w in question]
+    question = [stemmer.stem(w) for w in question[1:]]
 
     # lambdas to be tuned later
-    lambda1 = 0.7 # lambda for the tf-idf written above
-    lambda2 = 0.3 # lambda for the bm25 score from gensim bm25
+    lambda1 = 0.9 # lambda for the tf-idf written above
+    lambda2 = 0.02 # lambda for the bm25 score from gensim bm25
     bm25res = getbm25(question, file)
-
     for sentence in bm25res:
         s = sentence[0]
         bm25sim = sentence[1]
         ssw = [stemmer.stem(w.lower()) for w in word_tokenize(s)]
         thisSim = similarity(question,ssw,idfDict)
-        sims.append((s, (lambda1*thisSim+lambda2*bm25sim)))
+        # print(thisSim, bm25sim)
+        sims.append((s, (lambda1 * thisSim + lambda2 * bm25sim)))
+
     sortedSim = sorted(sims, key=lambda sentence: sentence[1],reverse = True)
-    return (sortedSim[0:5])
+    filtered = list(filter(lambda sentence: sentence [1] > 1.5, sortedSim))
+    if filtered == []:
+        return sortedSim[0:5]
+    else:
+        return filtered[0:5]
 
 def questionType(q):
     # function that takes in a list of tokens to get the question type
@@ -173,28 +183,31 @@ def whenToAns(qtokens,rawAns):
     merged = merge(tagged)
     return merged['DATE'][0] + "."
 
+def dist(qtokens,person,rawAns):
+    ansTokL = [tok.lower() for tok in word_tokenize(rawAns)]
+    if person.lower() in ansTokL:
+        personI = ansTokL.index(person.lower())
+    l = []
+    for i in range(len(qtokens)):
+        tok = qtokens[i]
+        if tok in ansTokL:
+            findRes = ansTokL.index(tok.lower())
+            l.append(findRes)
+    res = 0
+    for n in l:
+        res += abs(personI - n)
+    return res
+
 def whoToAns(qtokens,rawAns):
     rawToks = word_tokenize(rawAns)
     # well  I made this up :( maybe use dependency parsing?
-    def dist(qtokens,person,rawAns):
-        personI = rawAns.find(person)
-        l = []
-        for i in range(len(qtokens)):
-            tok = qtokens[i]
-            findRes = rawAns.lower().find(tok.lower())
-            if findRes != -1:
-                l.append(findRes)
-        res = 0
-        for n in l:
-            res += abs(personI - n)
-        return res
+
     tagged = NERTagger.tag(rawToks)
     merged = merge(tagged)
     people = []
     for (w,t) in tagged:
         if t == "PERSON":
             people.append(w)
-    # print(people)
     if len(people) == 0:
         for pronoun in ['he','she','that','this']:
             if pronoun in rawAns.lower():
@@ -204,7 +217,6 @@ def whoToAns(qtokens,rawAns):
         bestDist = None
         for person in people:
             d = dist(qtokens,person,rawAns)
-            # print(d)
             if bestDist == None or d < bestDist:
                 bestDist = d 
                 bestPerson = person
@@ -213,7 +225,30 @@ def whoToAns(qtokens,rawAns):
                 return p + '.'
         return bestPerson + "."
 
-def howToAns(qtokens,rawAns):
+def howToAns(qtokens,rawAns): 
+    if qtokens[1] == "many":   
+        parsed = [list(parse.triples()) for parse in depParse.raw_parse(rawAns)]
+        nums = []
+        for dep in parsed:
+            for (thing, rel, what) in dep:
+                if rel == 'nummod':
+                    nums.append(what[0])   
+        # pos tag raw Ans get cd
+        for (word, tag) in (pos_tag(word_tokenize(rawAns))):
+            if tag == "CD":
+                nums.append(word)
+        if nums != []:
+            bestNum = None
+            bestDist = None
+            for num in nums:
+                d = dist(qtokens,num,rawAns)
+                if bestDist == None or d < bestDist:
+                    bestDist = d 
+                    bestNum = num
+            if bestNum.isalpha():
+                if bestNum[0].isalpha():
+                    return bestNum[0].upper() + bestNum[1:] + "."
+                return bestNum + '.'
     return rawAns
 def whereToAns(qtokens,rawAns):
     return rawAns
@@ -237,20 +272,17 @@ def locateUsingNer(best5Sen, qtype, q):
     senLiteral = [sen[0] for sen in best5Sen]
     if qtype in ["yesno","what","which"]:
         return senLiteral[0]
-    if qtype == "who" or qtype == "where": 
+    if qtype == "who" or qtype == "where" or qtype == "when": 
         for sen in senLiteral:
             tagged = NERTagger.tag(word_tokenize(sen))
             for (word,tag) in tagged:
-                if qtype == "who" and tag == "PERSON":
+                if qtype == "who" and (tag == "PERSON" or word in PEOPLEPRO):
                     return sen
                 if qtype == "where" and tag == "LOCATION":
                     return sen
-    if qtype == "when":
-        for sen in senLiteral:
-            tagged = NERTagger.tag(word_tokenize(sen))
-            for (w,t) in tagged:
-                if t == "DATE":
+                if qtype == "when" and tag == "DATE":
                     return sen
+
     if qtype == "why":
         for sen in senLiteral:
             for w in word_tokenize(sen):
@@ -258,6 +290,7 @@ def locateUsingNer(best5Sen, qtype, q):
                     return sen
     
     if qtype == "how":
+        # print(best5Sen)
         qt = word_tokenize(q)
         if qt[1] == "many":
             for sen in senLiteral:
@@ -284,31 +317,28 @@ def testGet():
     for setNo in range(4):
         for articleNo in range(10):
             path = 'data/set%d/a%d.txt'%(setNo+1,articleNo+1)
-            # print(path)
             f = open(path)
             file = f.read().splitlines()
-            # print(getCategory(file))
 
 def postProcess(ans):
+    ans = word_tokenize(ans)
     for pronoun in PRONOUNS:
         if pronoun in ans:
-            ans.replace(pronoun, title)
-    return ans
+            index = ans.index(pronoun)
+            ans[index] = title
+    return "".join([" "+i if not i.startswith("'") and i not in string.punctuation else i for i in ans]).strip()
 
-def answer(file, question):
+
+def answerOne(file, question):
     global title
     f = open(file)
     ff = f.read().splitlines()                  # splitlines to avoid whitespace issue
     title = ff[0]
     category = getCategory(ff)
-
     qtokens = word_tokenize(question)           # tokenized question
-
     qtype = questionType(qtokens)
-    # print(qtype)
-
     best5Sen = (best5(ff, qtokens))   # string of the most similar sentence
-    # print(best5Sen[0][1])
+    # print(best5Sen)
     if best5Sen[0][1] > 2.5:        # instead of directly returning, maybe make NER a parameter
         rawAnswer = best5Sen[0][0]    # string of the most similar sentence
     else:
@@ -321,7 +351,6 @@ def answer(file, question):
     finalAns = None
     if len(word_tokenize(rawAnswer)) - len(qtokens) < 10:
         return(postProcess(rawAnswer))
-
     if questionType(qtokens) == "what":
         return(postProcess( whatToAns(qtokens,rawAnswer)))
     if questionType(qtokens) == "when":
@@ -329,79 +358,55 @@ def answer(file, question):
     if questionType(qtokens) == "why":
         return(postProcess( whyToAns(qtokens,rawAnswer)))
     if questionType(qtokens) == "how":
-        return(postProcess( howToAns(qtokens,rawAnswer)))
+        return(postProcess(howToAns(qtokens,rawAnswer)))
     if questionType(qtokens) == "who":
-        return(postProcess( whoToAns(qtokens,rawAnswer)))  
+        return(postProcess(whoToAns(qtokens,rawAnswer)))  
     if questionType(qtokens) == "where":
-        return(postProcess( whereToAns(qtokens,rawAnswer)))
+        return(postProcess(whereToAns(qtokens,rawAnswer)))
     if questionType(qtokens) == "which":
-        return(postProcess( whichToAns(qtokens,rawAnswer)))
+        return(postProcess(whichToAns(qtokens,rawAnswer)))
     else:
-        return(postProcess( otherToAns(qtokens,rawAnswer)))
+        return(postProcess(otherToAns(qtokens,rawAnswer)))
 
 
-# correctly answered questions:
+def answer(file, questions):
+    q = open(questions)
+    qs = q.read().splitlines()
+    for question in qs:
+        print(answerOne(file,question))
+
+# (answer('data/set1/a6.txt','ourqs/David_Beckham.txt'))
+# (answer('data/set3/a10.txt','ourqs/Esperanto.txt'))
+# (answer('data/set2/a8.txt','ourqs/Hercules_(constellation).txt'))
+# (answer('data/set3/a9.txt','ourqs/Java_(programming_language).txt'))
+# (answer('data/set1/a10.txt','ourqs/John_Terry.txt'))
+# (answer('data/set3/a6.txt','ourqs/Latin.txt'))
+# (answer('data/set3/a7.txt','ourqs/Python_(programming_language).txt'))
+# (answer('data/set2/a10.txt','ourqs/Crux.txt'))
 
 
-# print(answer('data/set2/a8.txt','Does Hercules have first or second magnitude stars?'))
-# print(answer('data/set2/a10.txt','Is Coalsack Nebula the most prominent dart nebula is the skies?'))
-# print(answer('data/set2/a8.txt','Is Hercules the fifth largest of the modern constellations?'))
-# print(answer('data/set1/a6.txt','Was Beckham the first England player ever to collect two red cards?'))
-# print(answer('data/set1/a9.txt','Does java combine the syntax for structured, generic and object-oriented programming?'))
-# print(answer('data/set3/a9.txt','Is the method name "main" a keyword in Java?'))
-# print(answer('data/set3/a9.txt','Does java use a garbage collector to manage memory?'))
-# print(answer('data/set3/a9.txt','Does java support C/C++ style pointer arithmetic?'))
+# problematic questions:
+'''
+1. How many music albums in Esperanto in the nineties?
+    sixties, seventies, eighties, nineties. cannot find the correct one. 
+2. when was Esperanto created?
+    late 1970s cannot be recognized as NER in the tagger
+3. How many countries used Esperanto?
+    spelling error, and also the lambdas need to be tuned
+4. where was John Terry born?
+    metadata
+5. Which team does John play for?
+6. What is CPython?
+    too short
+7. Is 10h 55.13m a right ascension coordinate?
+    ......
 
+4. semantics:
+    few - many
+    visible - see - invisible
+    Chinese - ??
+    trivial - significant
 
-# questions that can return the correct sentence but need to extract the answer.
-
-
-print(answer('data/set1/a6.txt','Who was Milan\'s chief executive in 2008?'))
-# print(answer('data/set2/a8.txt','How far away is Mu Herculis from Earth?'))
-# print(answer('data/set2/a8.txt','What are the two bright globular clusters contained in Hercules?'))
-# print(answer('data/set3/a6.txt','Where is the Latin alphabet derived from?'))
-# print(answer('data/set3/a7.txt','How are conditional expressions written in Python?'))
-
-# print(answer('data/set3/a10.txt','why people can create new words of Esperanto?'))
-print(answer('data/set3/a10.txt','Why Zamenhof created Esperanto?'))
-# print(answer('data/set3/a10.txt','How many music albums in Esperanto in the nineties?'))
-# print(answer('data/set3/a10.txt','What is the most widely spoken constructed language in the world?'))
-
-# print(answer('data/set3/a9.txt','What is WORA'))
-# print(answer('data/set3/a9.txt','Which version of java is supported for free by Oracle'))
-# print(answer('data/set3/a10.txt','How many countries used Esperanto?'))
-
-# questions that cannot
-# main problems: questions too short to match. NER?
-# print(answer('data/set1/a10.txt','where was John Terry born?'))
-# print("")
-# print("Q:what formed six diphthongs?")
-
-# print(answer('data/set3/a10.txt','what formed six diphthongs?'))
-# print("Q:when was Esperanto created?")
-
-print(answer('data/set3/a10.txt','when was Esperanto created?'))
-
-# print("Q：How many native speakers speaker Esperanto?")
-
-
-# print(answer('data/set3/a10.txt','How many native speakers speaker Esperanto?'))
-# print("Q：what does Esperanto\'s name derive from?")
-
-# print(answer('data/set3/a10.txt','what does Esperanto\'s name derive from?'))
-
-# print(answer('data/set3/a9.txt','How many reported developers for java?'))
-# print("How many reported developers for java?")
-
-# print(answer('data/set2/a10.txt','What is Crus commonly known as?'))
-
-# # incorrect "second"
-# print(answer('data/set3/a9.txt','What is the second latest version of Java?'))
-
-# # incorrect answer, few vs many
-# print(answer('data/set3/a9.txt','Does java have as many low-level facilities as C and C++?'))
-
-
-
+'''
 
 
